6292c745ac620bb929eaf662dc0c58b6c40883a6
Gaston Muñoz <gaston.m@flydevs.com>
2021-11-09 01:59:52 -0300

    Eliminado titulo del footer

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -3,9 +3,6 @@ import React from 'react';
 const Footer = () => {
     return ( 
         <footer className="footer bg-primary text-white">
-            <h6>
-              Gastón Muñoz for Guarnic - © 2020
-            </h6>
         </footer>
      );
 }
